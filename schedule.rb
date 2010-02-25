f6cf9d3ba7e2a7ace0d224b8acd39ec89d751957
abikounso <abikounso@gmail.com>
2010-02-25 14:40:04 +0900

    rubyで全面書き直し

--- schedule.rb
+++ schedule.rb
@@ -4,42 +4,51 @@ require "rubygems"
 require "active_support"
 time = 1.minutes.since Time.now
 
+def register_job(j = { :month => "*", :day => "*", :week => "1-5", :hour => "*", :min => "*", :wav => "" })
+  [j[:min], j[:hour], j[:day], j[:month], j[:week], "aplay", "#{ Dir.pwd }/wav/#{ j[:wav] }.wav", "\n"].join(" ")
+end
+
 JOBS =
 [
-["20 8 * 4-12 1-5", "chaim"],
-["0 9,12,17,18 * * 1-6", "chaim"],
-["56 8 * * 1-5", "manner_mode"],
-["0 13 * * 1,3,5", "radio_exercise01"],
-["0 13 * * 2,4", "radio_exercise02"],
-["1 17 * 6-9 4", "all_cleaning"],
-["4 13 * 1-5,10-12 4", "all_cleaning"],
-["45 20 * 1 1-5", "before_15minutes"],
-["58 20 * 1 1-5", "hotaru.wav"],
-["45 20 1-25 5 1-5", "before_15minutes"],
-["58 20 1-25 5 1-5", "hotaru"],
-["45 19 26-31 5 1-5", "before_15minutes"],
-["58 19 26-31 5 1-5", "hotaru"],
-["45 19 1-15 2 1-5", "before_15minutes"],
-["58 19 1-15 2 1-5", "hotaru"],
-["45 21 16-29 2 1-5", "before_15minutes"],
-["58 21 16-29 2 1-5", "hotaru"],
-["45 21 1-15 3 1-5", "before_15minutes"],
-["58 21 1-15 3 1-5", "hotaru"],
-["45 19 16-31 3 1-5", "before_15minutes"],
-["58 19 16-31 3 1-5", "hotaru"],
-["45 19 * 4,6-12 1-5", "before_15minutes"],
-["58 19 * 4,6-12 1-5", "hotaru"]#,
-#["#{ time.min } #{ time.hour } * * *", "chaim"]
+register_job(:month => "4-12"                                   , :hour => "8"           , :min => "20", :wav => "chaim"),
+register_job(                                   :week => "1-6"  , :hour => "0 9,12,17,18", :min => "0" , :wav => "chaim"),
+register_job(                                                     :hour => "8"           , :min => "56", :wav => "manner_mode"),
+register_job(                                   :week => "1,3,5", :hour => "13"          , :min => "0" , :wav => "radio_exercise01"),
+register_job(                                   :week => "2,4"  , :hour => "13"          , :min => "0" , :wav => "radio_exercise02"),
+register_job(:month => "6-9"                  , :week => "4"    , :hour => "17"          , :min => "1" , :wav => "all_cleaning"),
+register_job(:month => "1-5,10-12"            , :week => "4"    , :hour => "13"          , :min => "4" , :wav => "all_cleaning"),
+register_job(                                                     :hour => "8"           , :min => "56", :wav => "before_15minutes"),
+register_job(:month => "2"   , :day => "1-7"                    , :hour => "19"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "2"   , :day => "8-29"                   , :hour => "21"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "2"   , :day => "8-29"                   , :hour => "21"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "3"   , :day => "1-15"                   , :hour => "21"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "1"                                      , :hour => "20"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "1"                                      , :hour => "20"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "2"   , :day => "1-7"                    , :hour => "19"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "2"   , :day => "1-7"                    , :hour => "19"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "2"   , :day => "8-29"                   , :hour => "21"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "2"   , :day => "8-29"                   , :hour => "21"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "3"   , :day => "1-15"                   , :hour => "21"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "3"   , :day => "1-15"                   , :hour => "21"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "3"   , :day => "16-31"                  , :hour => "20"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "3"   , :day => "16-31"                  , :hour => "20"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "4"                                      , :hour => "19"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "4"                                      , :hour => "19"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "5"   , :day => "1-5"                    , :hour => "19"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "5"   , :day => "1-5"                    , :hour => "19"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "5"   , :day => "6-25"                   , :hour => "20"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "5"   , :day => "6-25"                   , :hour => "20"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "5"   , :day => "26-31"                  , :hour => "19"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "5"   , :day => "26-31"                  , :hour => "19"          , :min => "58", :wav => "hotaru"),
+register_job(:month => "6-12"                                   , :hour => "19"          , :min => "45", :wav => "before_15minutes"),
+register_job(:month => "6-12"                                   , :hour => "19"          , :min => "58", :wav => "hotaru")#,
+#register_job(:hour => time.hour.to_s, :min => time.min.to_s, :wav => "chaim")
 ]
-
-def play_wav(timing, name)
-  "#{ timing } aplay #{ Dir.pwd }/wav/#{ name }.wav"
-end
 
 user = Dir.entries("/home").reject { |d| /\.|\.\./ =~ d }
 cron = "/var/spool/cron/crontabs/#{ user }"
 File.open(cron, "w") do |line|
-  JOBS.each { |job| line.write(play_wav(job[0], job[1]) + "\n") }
+  JOBS.each { |job| line.write(job) }
 end
 require "fileutils"
 FileUtils.chmod(0600, cron, :verbose => true)
